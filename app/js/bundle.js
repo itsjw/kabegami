15aadd19496d6bbd1f2114888a1b6de7c84e5869
Josh Castle <joshrcastle@gmail.com>
2017-12-11 09:40:28 -0600

    ...

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -4,12 +4,14 @@
 	var VueRouter = require("vue-router");
 	Vue.use(VueRouter);
 
+	var routes = [];
+	var router = new VueRouter({routes});
+
 	window.onload = function(){
 		var app = new Vue({
 			el: "#app",
+			router,
 		});
-
-		console.log("ready!");
 	};
 })();
 
--- app/js/src/index.js
+++ app/js/src/index.js
@@ -3,9 +3,13 @@
 	var VueRouter = require("vue-router");
 	Vue.use(VueRouter);
 
+	var routes = [];
+	var router = new VueRouter({routes});
+
 	window.onload = function(){
 		var app = new Vue({
 			el: "#app",
+			router,
 		});
 	};
 })();
